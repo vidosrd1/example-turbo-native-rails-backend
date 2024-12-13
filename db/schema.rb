44fced1979b8222f384ae645ea0c807c0ac8c5f5
Srđan Vidović <vidosrd@gmail.com>
2024-12-13 13:22:56 +0100

    1

--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,6 @@ ActiveRecord::Schema[8.0].define(version: 2022_06_14_204924) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "authentication_token"
-    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
